94f473abf7a6b0bb9cd988e3595415f50f5bb53a
Caio Gomes <gcaio7463@gmail.com>
2023-05-27 12:37:09 -0300

    Adding fetch to connect two-factor-authentication API to authenticate user

--- login/login.js
+++ login/login.js
@@ -1,18 +1,54 @@
 const status_box = document.querySelector('#status_box')
-const email_input = document.querySelector("#email_input")
+const username_input = document.querySelector("#username_input")
 const password_input = document.querySelector('#password_input')
 const login_button = document.querySelector('#login_button')
 
 login_button.addEventListener('click', event => {
     event.preventDefault()
-    const email = email_input.value.trim().replace(/\s/g, '')
+    const username = username_input.value.trim().replace(/\s/g, '')
     const password = password_input.value.trim().replace(/\s/g, '')
-    if(verifyEmail(email) && verifyPassword(password)) {
-        setSuccessfullStatus('Ok! Redirecting...')
-        return
+    if(verifyUsername(username) && verifyPassword(password)) {
+        loginUser(username, password)
     }
-    setErrorStatus('Something gone wrong, verify the credentials or try again later.')
 })
+
+async function loginUser(username, password) {
+    const auth = {
+        username: username, 
+        password: password
+    };
+    const options = {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        }, 
+        body: JSON.stringify(auth)
+    };
+    const response = await fetch("http://localhost:8080/users/login", options);
+    const data = await response.json();
+    if(response.ok) {
+        console.log(data.token);
+        setSuccessfullStatus("Ok! Redirecting...")
+        return
+    } else {
+        setErrorStatus(data.message)
+    }
+}
+
+function verifyUsername(username) {
+    if(username.length >= 3 && username.length <= 50) {
+        return true
+    }
+    setErrorStatus('Something gone wrong, NAME is invalid!')
+    return false
+}
+
+function verifyPassword(password) {
+    if(password.length >= 6) {
+        return true
+    }
+    return false
+}
 
 function setErrorStatus(message) {
     status_box.style.backgroundColor = '#8B0000'
@@ -26,23 +62,4 @@ function setSuccessfullStatus(message) {
     status_box.style.width = '350px'
     status_box.style.height = '50px'
     status_box.innerHTML = '<p id="status_text">' + message + '</p>'
-}
-
-function verifyEmail(email) {
-    if(email.length >= 5 && email.includes('@') && email.includes('.')) {
-        const indexAt = email.indexOf('@')
-        const indexPeriod = email.indexOf('.')
-        const lengthBetweenAtAndPeriod = email.substring(indexAt + 1, indexPeriod).length
-        if(indexAt > 0 && indexPeriod + 1 < email.length && lengthBetweenAtAndPeriod > 0) {
-            return true
-        }
-    }
-    return false
-}
-
-function verifyPassword(password) {
-    if(password.length >= 6) {
-        return true
-    }
-    return false
 }
