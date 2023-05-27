0696e79190139653f509b837ba04416faf976b64
Caio Gomes <gcaio7463@gmail.com>
2023-05-27 12:41:22 -0300

    Ajusting code style

--- create/create.js
+++ create/create.js
@@ -1,17 +1,17 @@
-const status_box = document.querySelector('#status_box')
-const username_input = document.querySelector('#username_input')
-const email_input = document.querySelector('#email_input')
-const password_input = document.querySelector('#password_input')
-const confirm_password_input = document.querySelector('#confirm_password_input')
-const create_button = document.querySelector('#create_button')
+const status_box = document.querySelector('#status_box');
+const username_input = document.querySelector('#username_input');
+const email_input = document.querySelector('#email_input');
+const password_input = document.querySelector('#password_input');
+const confirm_password_input = document.querySelector('#confirm_password_input');
+const create_button = document.querySelector('#create_button');
 
 create_button.addEventListener('click', event => {
-    event.preventDefault()
-    const username = username_input.value.trim()
-    const email = email_input.value.trim().replace(/\s/g, '')
-    const phone = phone_input.value.trim().replace(/\s/g, '')
-    const password = password_input.value.trim().replace(/\s/g, '')
-    const confirm_password = confirm_password_input.value.trim().replace(/\s/g, '')
+    event.preventDefault();
+    const username = username_input.value.trim();
+    const email = email_input.value.trim().replace(/\s/g, '');
+    const phone = phone_input.value.trim().replace(/\s/g, '');
+    const password = password_input.value.trim().replace(/\s/g, '');
+    const confirm_password = confirm_password_input.value.trim().replace(/\s/g, '');
     if(verifyUserame(username) && 
     verifyEmail(email) && 
     verifyPassword(password) && 
@@ -46,52 +46,51 @@ async function createUser(email, password, username) {
 
 function verifyUserame(username) {
     if(username.length >= 3 && username.length <= 50) {
-        return true
+        return true;
     }
-    console.log('HEY')
-    setErrorStatus('Something gone wrong, NAME is invalid!')
-    return false
+    setErrorStatus('Something gone wrong, NAME is invalid!');
+    return false;
 }
 
 function verifyEmail(email) {
     if(email.length >= 5 && email.includes('@') && email.includes('.')) {
-        const indexAt = email.indexOf('@')
-        const indexPeriod = email.indexOf('.')
-        const lengthBetweenAtAndPeriod = email.substring(indexAt + 1, indexPeriod).length
+        const indexAt = email.indexOf('@');
+        const indexPeriod = email.indexOf('.');
+        const lengthBetweenAtAndPeriod = email.substring(indexAt + 1, indexPeriod).length;
         if(indexAt > 0 && indexPeriod + 1 < email.length && lengthBetweenAtAndPeriod > 0) {
-            return true
+            return true;
         }
     }
-    setErrorStatus('Something gone wrong, EMAIL is invalid!')
-    return false
+    setErrorStatus('Something gone wrong, EMAIL is invalid!');
+    return false;
 }
 
 function verifyPassword(password) {
     if(password.length >= 6) {
-        return true
+        return true;
     }
-    setErrorStatus('Something gone wrong, PASSWORD has to be longer!')
-    return false
+    setErrorStatus('Something gone wrong, PASSWORD has to be longer!');
+    return false;
 }
 
 function confirmPassword(password, confirm_password) {
     if(password === confirm_password) {
-        return true
+        return true;
     }
-    setErrorStatus('Something gone wrong, PASSWORD must be the same!')
-    return false
+    setErrorStatus('Something gone wrong, PASSWORD must be the same!');
+    return false;
 }
 
 function setErrorStatus(message) {
-    status_box.style.backgroundColor = '#8B0000'
-    status_box.style.width = '350px'
-    status_box.style.height = '50px'
-    status_box.innerHTML = '<p id="status_text">' + message + '</p>'
+    status_box.style.backgroundColor = '#8B0000';
+    status_box.style.width = '350px';
+    status_box.style.height = '50px';
+    status_box.innerHTML = '<p id="status_text">' + message + '</p>';
 }
 
 function setSuccessfullStatus(message) {
-    status_box.style.backgroundColor = '#008000'
-    status_box.style.width = '350px'
-    status_box.style.height = '50px'
-    status_box.innerHTML = '<p id="status_text">' + message + '</p>'
+    status_box.style.backgroundColor = '#008000';
+    status_box.style.width = '350px';
+    status_box.style.height = '50px';
+    status_box.innerHTML = '<p id="status_text">' + message + '</p>';
 }
--- login/login.js
+++ login/login.js
@@ -1,14 +1,14 @@
-const status_box = document.querySelector('#status_box')
-const username_input = document.querySelector("#username_input")
-const password_input = document.querySelector('#password_input')
-const login_button = document.querySelector('#login_button')
+const status_box = document.querySelector('#status_box');
+const username_input = document.querySelector("#username_input");
+const password_input = document.querySelector('#password_input');
+const login_button = document.querySelector('#login_button');
 
 login_button.addEventListener('click', event => {
-    event.preventDefault()
-    const username = username_input.value.trim().replace(/\s/g, '')
-    const password = password_input.value.trim().replace(/\s/g, '')
+    event.preventDefault();
+    const username = username_input.value.trim().replace(/\s/g, '');
+    const password = password_input.value.trim().replace(/\s/g, '');
     if(verifyUsername(username) && verifyPassword(password)) {
-        loginUser(username, password)
+        loginUser(username, password);
     }
 })
 
@@ -28,38 +28,38 @@ async function loginUser(username, password) {
     const data = await response.json();
     if(response.ok) {
         console.log(data.token);
-        setSuccessfullStatus("Ok! Redirecting...")
-        return
+        setSuccessfullStatus("Ok! Redirecting...");
+        return;
     } else {
-        setErrorStatus(data.message)
+        setErrorStatus(data.message);
     }
 }
 
 function verifyUsername(username) {
     if(username.length >= 3 && username.length <= 50) {
-        return true
+        return true;
     }
-    setErrorStatus('Something gone wrong, NAME is invalid!')
-    return false
+    setErrorStatus('Something gone wrong, NAME is invalid!');
+    return false;
 }
 
 function verifyPassword(password) {
     if(password.length >= 6) {
-        return true
+        return true;
     }
-    return false
+    return false;
 }
 
 function setErrorStatus(message) {
-    status_box.style.backgroundColor = '#8B0000'
-    status_box.style.width = '350px'
-    status_box.style.height = '65px'
-    status_box.innerHTML = '<p id="status_text">' + message + '</p>'
+    status_box.style.backgroundColor = '#8B0000';
+    status_box.style.width = '350px';
+    status_box.style.height = '65px';
+    status_box.innerHTML = '<p id="status_text">' + message + '</p>';
 }
 
 function setSuccessfullStatus(message) {
-    status_box.style.backgroundColor = '#008000'
-    status_box.style.width = '350px'
-    status_box.style.height = '50px'
-    status_box.innerHTML = '<p id="status_text">' + message + '</p>'
+    status_box.style.backgroundColor = '#008000';
+    status_box.style.width = '350px';
+    status_box.style.height = '50px';
+    status_box.innerHTML = '<p id="status_text">' + message + '</p>';
 }
