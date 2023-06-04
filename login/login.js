900a5fa4f711932188c80a606564049ff1f92e25
Caio Gomes <gcaio7463@gmail.com>
2023-06-04 11:46:52 -0300

    Adjusting code style and rename variables

--- login/login.js
+++ login/login.js
@@ -1,17 +1,22 @@
-const status_box = document.querySelector('#status_box');
-const username_input = document.querySelector("#username_input");
-const password_input = document.querySelector('#password_input');
-const login_button = document.querySelector('#login_button');
+//Inputs
+const usernameInput = document.querySelector("#username_input");
+const passwordInput = document.querySelector('#password_input');
 
-login_button.addEventListener('click', event => {
+//Others
+const statusBox = document.querySelector('#status_box');
+const loginBtn = document.querySelector('#login_btn');
+
+//Adding avent listener to login button
+loginBtn.addEventListener('click', event => {
     event.preventDefault();
-    const username = username_input.value.trim().replace(/\s/g, '');
-    const password = password_input.value.trim().replace(/\s/g, '');
+    const username = usernameInput.value.trim().replace(/\s/g, '');
+    const password = passwordInput.value.trim().replace(/\s/g, '');
     if(verifyUsername(username) && verifyPassword(password)) {
         loginUser(username, password);
     }
 })
 
+//Call API to login user
 async function loginUser(username, password) {
     const auth = {
         username: username, 
@@ -24,17 +29,27 @@ async function loginUser(username, password) {
         }, 
         body: JSON.stringify(auth)
     };
-    const response = await fetch("http://localhost:8080/users/login", options);
-    const data = await response.json();
-    if(response.ok) {
-        console.log(data.token);
-        setSuccessfullStatus("Ok! Redirecting...");
-        return;
-    } else {
-        setErrorStatus(data.message);
+    try {
+        setWarningStatus("Trying to login...");
+        const response = await fetch("http://localhost:8080/users/login", options);
+        const data = await response.json();
+        if(response.ok) {
+            localStorage.setItem("token", data.token);
+            setSuccessfullStatus("Ok! Redirecting...");
+            setTimeout(function() {
+                window.location.replace("file:///C:/Users/caiog/VisualCodeProjects/login-page/home/home.html");
+            }, 1000);
+            return;
+        } else {
+            setErrorStatus(data.message);
+        }
+    } catch(e) {
+        setErrorStatus('Something gone wrong, try again later.')
+        console.error(e);
     }
 }
 
+//Verifying fields
 function verifyUsername(username) {
     if(username.length >= 3 && username.length <= 50) {
         return true;
@@ -47,19 +62,28 @@ function verifyPassword(password) {
     if(password.length >= 6) {
         return true;
     }
+    setErrorStatus('Something gone wrong, PASSWORD has to be longer!');
     return false;
 }
 
+//Setting status box
 function setErrorStatus(message) {
-    status_box.style.backgroundColor = '#8B0000';
-    status_box.style.width = '350px';
-    status_box.style.height = '65px';
-    status_box.innerHTML = '<p id="status_text">' + message + '</p>';
+    statusBox.style.backgroundColor = '#8B0000';
+    statusBox.style.width = '350px';
+    statusBox.style.height = '65px';
+    statusBox.innerHTML = '<p id="status_text">' + message + '</p>';
+}
+
+function setWarningStatus(message) {
+    statusBox.style.backgroundColor = "#FFAA33";
+    statusBox.style.width = "350px";
+    statusBox.style.height = '65px';
+    statusBox.innerHTML = '<p id="status_text">' + message + '</p>';
 }
 
 function setSuccessfullStatus(message) {
-    status_box.style.backgroundColor = '#008000';
-    status_box.style.width = '350px';
-    status_box.style.height = '50px';
-    status_box.innerHTML = '<p id="status_text">' + message + '</p>';
+    statusBox.style.backgroundColor = '#008000';
+    statusBox.style.width = '350px';
+    statusBox.style.height = '50px';
+    statusBox.innerHTML = '<p id="status_text">' + message + '</p>';
 }
